99e4b22f8d23ad8be30af42be9e55fed1e46690a
aditya soni <dev.aditya270325@gmail.com>
2026-02-22 00:19:55 +0530

    initial just for starting the server

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
+app.use(cors())
+
+app.get('/news', async (req, res) => {
+    try{
+    const { country = 'in', category = 'top', q, page } = req.query
+
+    const URL = `https://newsdata.io/api/1/latest?apikey=${process.env.API2}&language=en&country=${country}&category=${category}${q ? `&q=${q}` : ""}${page ? `&page=${page}` : ""}`;
+    
+    const data = await fetch(URL)
+    const parsedData = await data.json()
+    
+    res.json(parsedData)
+    } catch(err){
+        res.status(500).json({error: "Something went wrong"})
+    }
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server listning on http://localhost:${PORT}`);
 })
