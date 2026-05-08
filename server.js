27ed391afbdb9b9ffe7e565a6e7136d3d47922a7
aditya soni <dev.aditya270325@gmail.com>
2026-05-09 02:29:45 +0530

    cors updated

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    // "https://your-frontend-domain.vercel.app"
+    "https://indianews-flame.vercel.app/"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
