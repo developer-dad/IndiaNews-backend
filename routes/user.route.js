754dd898ec7159c24ea430ff444e172d13832da5
aditya soni <dev.aditya270325@gmail.com>
2026-05-09 20:58:57 +0530

    User fetching auth middleware added

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { fetchUser, login, resetPassword, sendOTPController, signup } from "../controllers/user.controller.js";
+import { authMiddleware } from "../middleware/user.middleware.js";
 
 const userRouter = express.Router()
 
@@ -9,8 +10,8 @@ userRouter.post('/signup', signup)
 // LogIn existing User => api/v1/user/login
 userRouter.post('/login', login)
 
-// To get User Details
-userRouter.get('/fetch-user', fetchUser)
+// To get User Details => api/vi/user/fetch-user
+userRouter.get('/fetch-user', authMiddleware, fetchUser)
 
 // Send OTP using NodeMailer => api/v1/user/send-otp
 userRouter.post('/send-otp', sendOTPController)
